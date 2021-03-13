270fb38ec0d5f6e76dddb183cfb70951777eef55
ruther <ruther2525@gmail.com>
2021-03-14 00:50:32 +0900

    まちがってmainでやってたのでそのままcommitしますすいませんstashはしません

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,59 @@
+import discord
 from discord.ext import commands
 import os
 import traceback
+import asyncio
+import psycopg2
+from psycopg2.extras import DictCursor
 
-bot = commands.Bot(command_prefix=':)')
+conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
+with conn.cursor(cursor_factory=DictCursor) as cur:
+    cur.excute('SELECT Prefix FROM BotDetail;')
+    rows = cur.fetchall()
+    print(rows)
+    prefix = str(rows['prefix'])
+
+bot = commands.Bot(command_prefix=prefix)
 token = os.environ['DiscordBotToken']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    print(error_msg)
+InExtensions = [
+    'reaction_roles'
+]
+
+loop = asyncio.new_event_loop()
+
+async def run():
+    bot = GenkaiMainClass()
+    try:
+        await bot.start(token)
+    except KeyboardInterrupt:
+        await bot.logout()
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+class GenkaiMainClass(commands.bot):
+
+    def __init__(self):
+        super.__init__(command_prefix=commands.when_mentioned_or(prefix), loop=loop)
+    
+    async def on_ready(self):
+        for extension in InExtensions:
+            try:
+                self.load_extension(f'cogs.{extension}')
+            except commands.ExtensionAlreadyLoaded:
+                self.reload_extension(f'cogs.{extension}')
+        await self.change_presence(activity=discord.Game(name=f'{prefix}help'))
+    
+    async def on_command_error(self, ctx, error1):
+        if isinstance(error1, (commands.CommandNotFound, commands.CommandInvokeError)):
+            return
 
 
-bot.run(token)
+if __name__ == '__main__':
+    try:
+        main_task = loop.create_task(run())
+        loop.run_until_complete(main_task)
+        loop.close()
+
+    except Exception as error:
+        print('Error:\n  ' + traceback.format_exc())
