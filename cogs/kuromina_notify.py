a2c23f14e2f71a9260dd3a682e82690cae0bcfe8
ruther <ruther2525@gmail.com>
2021-04-30 22:35:04 +0900

    ✏くろみな通知の完成＆停止処置

--- cogs/kuromina_notify.py
+++ cogs/kuromina_notify.py
@@ -1,5 +1,6 @@
 import os
 from discord.ext import commands
+import discord
 import psycopg2
 import asyncpg
 from discord.ext.commands import bot
@@ -24,14 +25,37 @@ class kuromina_notify(commands.Cog):
                 async_conn = await asyncpg.connect(os.environ['DATABASE_URL'])
                 sql = f"INSERT INTO KuroMinaNotify(id) VALUES ({user_id});"
                 await async_conn.execute(sql)
-                self.KuroMinaNotifyList.append({'id': int(user_id)})
-                await ctx.send('たぶんできた')
+                self.KuroMinaNotifyList.append(tuple([user_id]))
+                user = self.bot.get_user(user_id)
+                embed = discord.Embed()
+                embed.add_field(name='できた', value='{} さん'.format(user.mention))
+                await ctx.send(embed=embed)
                 print(repr(self.KuroMinaNotifyList))
+    
+    @commands.command()
+    async def krmn_list(self, ctx):
+        embed = discord.Embed(title='くろみな通知 | 通知一覧', color=discord.Colour.dark_orange())
+        print(self.KuroMinaNotifyList)
+        for a in self.KuroMinaNotifyList:
+            print(a)
+            user = self.bot.get_user(int(a[0]))
+            if user is not None:
+                embed.add_field(name=str(user.name), value=str(user.id))
+        await ctx.send(embed=embed)
 
+    @commands.command()
+    async def dm_totu(self, ctx, message: str):
+        for a in self.KuroMinaNotifyList:
+            user = self.bot.get_user(int(a[0]))
+            if user is not None:
+                user.send(message)
+        
+        await ctx.reply('できたよ')
+
+    """
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        print(repr(member))
-        if after.channel is not None and (member.id == 227375367990542338 or member.id == 553991444172374027):
+        if before.channel is None and after.channel is not None and (member.id == 227375367990542338 or member.id == 553991444172374027):
             kuromina_tmp = 0
             for a in after.channel.members:
                 if a.id == 227375367990542338 or a.id == 553991444172374027:
@@ -39,8 +63,11 @@ class kuromina_notify(commands.Cog):
             
             if kuromina_tmp > 1:
                 for row in self.KuroMinaNotifyList:
-                    user = self.bot.get_user(row.id)
-                    await user.send('【くろみな通知】くろみなが限界創作村の __' + str(after.channel.name) + '__ に参加しました')
-
+                    user = self.bot.get_user(int(row[0]))
+                    try:
+                        await user.send('【くろみな通知】くろみなが限界創作村の __*' + str(after.channel.name) + '*__ に参加しました')
+                    except discord.Forbidden:
+                        print('User: "' + user.name + '" is not open DM, so bot cant send DM')
+        """
 def setup(bot):
     bot.add_cog(kuromina_notify(bot))
